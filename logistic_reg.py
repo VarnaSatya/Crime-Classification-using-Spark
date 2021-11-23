d72fd27e78dec404111613f9f5a06b8feea2a378
Swarnamalya A S <54075391+swarna0712@users.noreply.github.com>
2021-11-23 23:10:53 +0530

    Add files via upload

--- logistic_reg.py
+++ logistic_reg.py
@@ -13,6 +13,8 @@ from pyspark.ml.evaluation import ClusteringEvaluator
 from pyspark.ml.feature import VectorAssembler,StringIndexer
 from pyspark.ml.classification import LogisticRegression
 import os
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+
 os.environ["PYSPARK_PYTHON"] = "python3"
 '''OUR TARGET AND THEIR TARGET IS NOT THE SAME 
 WHICH IN TURN IS GIVING DIFFERENT NUMBERS IN THE DICTIONARY'''
@@ -41,7 +43,7 @@ for data in target:
 indexer = StringIndexer(inputCol="Category", outputCol="categoryIndex")
 indexed = indexer.fit(train_data).transform(train_data)
 
-indexed = indexed.withColumn("categoryIndex", indexed["categoryIndex"].cast(IntegerType()))
+indexed = indexed.withColumn("categoryIndex", indexed["categoryIndex"].cast('float'))
 indexed=indexed.withColumn("categoryIndex",col("categoryIndex") + lit(1))
 
 
@@ -49,25 +51,25 @@ indexer1 = StringIndexer(inputCol="DayOfWeek", outputCol="DayofWeekIndex")
 indexed1 = indexer1.fit(indexed).transform(indexed)
 
 
-indexed1 = indexed1.withColumn("DayofWeekIndex", indexed1["DayofWeekIndex"].cast(IntegerType()))
+indexed1 = indexed1.withColumn("DayofWeekIndex", indexed1["DayofWeekIndex"].cast('float'))
 indexed1=indexed1.withColumn("DayofWeekIndex",col("DayofWeekIndex") + lit(1))
 
 indexer2 = StringIndexer(inputCol="PdDistrict", outputCol="PdDistrictIndex")
 indexed2 = indexer2.fit(indexed1).transform(indexed1)
 
-indexed2 = indexed2.withColumn("PdDistrictIndex", indexed2["PdDistrictIndex"].cast(IntegerType()))
+indexed2 = indexed2.withColumn("PdDistrictIndex", indexed2["PdDistrictIndex"].cast('float'))
 indexed2=indexed2.withColumn("PdDistrictIndex",col("PdDistrictIndex") + lit(1))
 
 
 
 indexertest = StringIndexer(inputCol="DayOfWeek", outputCol="DayofWeekIndex")
 indexedtest = indexertest.fit(test_data).transform(test_data)
-indexedtest = indexedtest.withColumn("DayofWeekIndex", indexedtest["DayofWeekIndex"].cast(IntegerType()))
+indexedtest = indexedtest.withColumn("DayofWeekIndex", indexedtest["DayofWeekIndex"].cast('float'))
 indexedtest=indexedtest.withColumn("DayofWeekIndex",col("DayofWeekIndex") + lit(1))
 
 indexertest1 = StringIndexer(inputCol="PdDistrict", outputCol="PdDistrictIndex")
 indexedtest1 = indexertest1.fit(indexedtest).transform(indexedtest)
-indexedtest1 = indexedtest1.withColumn("PdDistrictIndex", indexedtest1["PdDistrictIndex"].cast(IntegerType()))
+indexedtest1 = indexedtest1.withColumn("PdDistrictIndex", indexedtest1["PdDistrictIndex"].cast('float'))
 indexedtest1=indexedtest1.withColumn("PdDistrictIndex",col("PdDistrictIndex") + lit(1))
 
 
@@ -79,34 +81,39 @@ indexed2=indexed2.drop('Category')
 
 indexedtest1=indexedtest1.drop('DayOfWeek')
 
-
-
-
-
-
+"""
+X_train = X_train.withColumn("DayofWeekIndex", 
+                                  X_train["DayofWeekIndex"]
+                                  .cast('float'))
+                                  """
+                                 
 features = ["DayofWeekIndex", "PdDistrictIndex",  "X", "Y"]
 
 
-
-X_train=np.array(indexed2.select(features).collect())
-
-y_train=np.array(indexed2.select("categoryIndex").collect()).reshape(-1)
-
-X_train=X_train.astype('float64')
+train,test,val=indexed2.randomSplit([0.7, 0.15, 0.15],seed=50)
 
 
-X_test=np.array(indexedtest1.select(features).collect())
-X_test=X_test.astype('float64')
+X_train=train.select(features)
+
+y_train=train.select("categoryIndex")
+
+#X_train=X_train.astype('float64')
+
+
+X_test=indexedtest1.select(features)
+#X_test=X_test.astype('float64')
+
 
 
 lr = LogisticRegression(featuresCol = 'features', labelCol = 'label', maxIter=10)
 lrModel = lr.fit(X_train)
-predictions = lrModel.transform(X_test)
+predictions = lrModel.transform(val)
 
-
+evaluator = MulticlassClassificationEvaluator(predictionCol="prediction")
+evaluator.evaluate(predictions)
 
 """lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
-lrModel = lr.fit(X_train)
+lrModel = lr.fit(X_train1)
 print("Coefficients: \n" + str(lrModel.coefficientMatrix))
 print("Intercept: " + str(lrModel.interceptVector))
 
@@ -148,6 +155,7 @@ recall = trainingSummary.weightedRecall
 print("Accuracy: %s\nFPR: %s\nTPR: %s\nF-measure: %s\nPrecision: %s\nRecall: %s"
       % (accuracy, falsePositiveRate, truePositiveRate, fMeasure, precision, recall))
 """
+
 
 
 
